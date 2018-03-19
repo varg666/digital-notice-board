ab773d71491795cdbf938919015bebdcb5381139
Oliver Weber <oliver@mondough.io>
2018-03-19 10:59:05 +0100

    es6 style and process.env for PORT fixes #32

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 4000
 
-app.get('/',function(req,res) {
+app.get('/',(req,res) => {
   res.send('Hello World!');
 });
 
-const server = app.listen(4000,function() {
-  console.log("Server listening on port http://localhost:4000");
+app.listen(PORT,() => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
