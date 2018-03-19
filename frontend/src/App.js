23032b017ecf408ea0fa77188d3ed096ff936838
Meralle <mar.ha17@outlook.com>
2018-03-19 14:51:49 +0100

    create the data object in MOdulesSideBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,50 +4,7 @@ import './App.css';
 import ModulesSideBar from './components/modules-side-bar/ModulesSideBar';
 
 class App extends Component {
-   constructor(props){
-      super(props);
-      this.state = {
-        data:
-      [
-        {
-          "id":0,
-          "name":"javaScript:The Definitive Guide, 6t Edition",
-          "release":"September 2010",
-          "amount":1,
-          "price":"2.99",
-          "liked" :false
-        },
-        {
-          "id":1,
-          "name":"Ruby on Rails: Up and Running",
-          "release":"March 2007",
-          "amount":1,
-          "price":"3.99",
-          "liked" :false  
-        },
-        {
-          "id":2,
-          "name":"Ruby on Rails: Up and Running",
-          "release":"March 2007",
-          "amount":1,
-          "price":"340.99",
-          "liked" :false  
-        },
-        {
-          "id":3,
-          "name":"Ruby on Rails: Up and Running",
-          "release":"March 2007",
-          "amount":1,
-          "price":"50.99",
-          "liked" :false  
-        }
-      ]
-    }
-  }
-
-
-
-
+  
 
   render() {
     return (
--- frontend/src/components/modules-side-bar/ModulesSideBar.js
+++ frontend/src/components/modules-side-bar/ModulesSideBar.js
@@ -4,6 +4,64 @@ import React, { Component } from 'react';
 
 
 class ModulesSideBar extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        data:
+        [
+    {
+        "type": "video",
+        "title": "The Rabbit and the turle",
+        "expiryDate": "01.07.2018",
+        "displayDate": "01.07.2018",
+        "description": "",
+        "content":
+        {
+            "src": "https://pixabay.com/en/background-image-colorful-art-967820/"
+        }
+    },
+    {
+        "type": "Image Gallery",
+        "title": "Startup Trip",
+        "description": "Team work video",
+        "expiryDate": "01.07.2018",
+        "displayDate": "01.07.2018",
+        "content":
+        {
+            "src": ["https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/"]
+        }
+    },
+    {
+        "type": "Code Snippet",
+        "title": "The Rabbit and the turle",
+        "description": "Team work video",
+        "expiryDate": "01.07.2018",
+        "displayDate": "01.07.2018",
+        "content":
+        {
+            "codeImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
+            "authorImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
+            "authorName": "Aemal Surname"
+        }
+    },
+    {
+        "type": "Event",
+        "title": "19th of March to 22nd march",
+        "subtitle": "Devugees Project Week",
+        "description": "Team work video",
+        "expiryDate": "01.07.2018",
+        "displayDate": "01.07.2018",
+        "content": {
+                "street": "Vulkanstr. 1",
+                "postal": "11000",
+                "city": "Berlin"
+            }
+        }]
+
+     }   
+}
+
+
   render() {
     return (
       <div className="ModulesSideBar">
