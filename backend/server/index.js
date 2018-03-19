a376d43d8003f489dc9ad3bb09a38a2b35bddd8f
Oliver Weber <oliver@mondough.io>
2018-03-19 10:44:37 +0100

    clean starter code

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/',function(req,res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(4000,function(res, req) {
-console.log(req, res)
-  console.log("Server listening on port http://localhost:4000")
+const server = app.listen(4000,function() {
+  console.log("Server listening on port http://localhost:4000");
 });
