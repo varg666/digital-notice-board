05193178dc9386a37b1a2b9b8ae3715b587e52ad
Marta <martadipuccio@me.com>
2018-03-19 16:06:48 +0100

    WIP form validation Login component

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,15 +3,42 @@ import React from 'react';
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loginData : {
+                email: "",
+                password:""
+            }
+        }
+        this.clickHandler = this.clickHandler.bind(this);
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     }
+
+
+    clickHandler(event) {
+        console.log(this.state)
+        console.log(event)
+        const loginDataCopy = {...this.state.loginData};
+        loginDataCopy[event.target.name] = event.target.value;
+        this.setState({loginData : loginDataCopy});
+    };
+        
+    validateForm: function() {      
+        if(this.state.loginData.email).value ==  {
+
+        }
+      };
+
+
+
+
+
+
         render(){
             return (
     <div className="container">    
                     <div>
                         <div>   
                             <h1>Sign Up</h1>
-          
-                    
 
                         </div>
         
@@ -23,7 +50,10 @@ class Login extends React.Component {
                                 <label>
                                 Email Address<span>*</span>
                                 </label>
-                                <input type="email"required autocomplete="off"/>
+                                <input 
+                                    value={this.state.loginData.email}
+                                    onChange={}
+                                    type="email"required autocomplete="off"/>
                             </div>
                         
                             <div>
@@ -33,7 +63,7 @@ class Login extends React.Component {
                                 <input type="password"required autocomplete="off"/>
                             </div>
                         
-                                <button>Log In</button>
+                                <button onClick={(e) => this.clickHandler(e)}>Log In</button>
                                 <p><a href="#">Register a new account</a></p>
                                 <p><a href="#">Forgot Password</a></p>
                         
@@ -45,12 +75,9 @@ class Login extends React.Component {
                     </div>
 
 
-
-
     </div>    
             );
         }
-
 
 
 }
