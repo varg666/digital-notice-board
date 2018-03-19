981ef36fc6de4dbee6fff60bea7893bbf3708285
AmerBdx <amerbdx@gmail.com>
2018-03-19 14:56:42 +0100

    Feature/video/disply_video

--- frontend/src/components/video/Video.js
+++ frontend/src/components/video/Video.js
@@ -1,19 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Video extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="bla" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Video = (props) => {
 
-export default Video;
+  return (
+    <iframe title={props.videoSrc} width="560" height="315" src={props.videoSrc} frameBorder="0" allowFullScreen></iframe>
+    
+  );
+};
+export default Video
