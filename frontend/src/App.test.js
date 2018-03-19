76e6f88f95aff12ad78d771d54d4be409e8bdeeb
Unknown <orwa.houry@outlook.de>
2018-03-19 16:40:22 +0100

    try the first test on app.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('test', () => {
+  it('start', () => {
+    const wrapper = shallow(<Video />);
+    const text = wrapper.find('h1').text();
+    console.log(text);
+    expect(text).toEqual('Welcome to React')
+  })
 });
