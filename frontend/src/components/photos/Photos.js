4df2f121c81cf2b2deb5fb5abf460a373b04b591
Unknown <roman_klester@t-online.de>
2018-03-20 12:46:00 +0100

    Created progressbar. Animation for slides and progressbar.

--- frontend/src/components/photos/Photos.js
+++ frontend/src/components/photos/Photos.js
@@ -6,13 +6,16 @@ class Photos extends Component {
   render() {
     return (
       <div className="photos">
-        <img className="animated zoomIn" src="http://via.placeholder.com/854x480" />
+        <img className="animated zoomIn" src="http://via.placeholder.com/857x480" />
         <div className="thumbnails">
           <img className="animated slideInRight" src="http://via.placeholder.com/115x70" />
           <img className="animated slideInDown" src="http://via.placeholder.com/115x70" />
           <img className="animated jackInTheBox" src="http://via.placeholder.com/115x70" />
           <img className="animated slideInUp" src="http://via.placeholder.com/115x70" />
           <img className="animated slideInLeft" src="http://via.placeholder.com/115x70" />
+        </div>
+        <div className="progress animated rotateInUpRight">
+          <div className="progressBar">0%</div>
         </div>
       </div>
     );
