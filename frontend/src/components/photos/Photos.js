7606cde5b547747557b4cdcb152b13732f40de6b
Unknown <roman_klester@t-online.de>
2018-03-20 15:00:16 +0100

    Wrote commentaries in Photos.js-file

--- frontend/src/components/photos/Photos.js
+++ frontend/src/components/photos/Photos.js
@@ -3,10 +3,14 @@ import './Photos.css';
 import '../../../node_modules/animate.css/animate.min.css'
 
 class Photos extends Component {
+
   render() {
     return (
+      // The main container
       <div className="photos">
+        {/*The big picture*/}
         <img className="animated zoomIn" src="http://via.placeholder.com/857x480" />
+      {/*The container with slides under the big picture*/}
         <div className="thumbnails">
           <img className="animated slideInRight" src="http://via.placeholder.com/115x70" />
           <img className="animated slideInDown" src="http://via.placeholder.com/115x70" />
@@ -14,6 +18,7 @@ class Photos extends Component {
           <img className="animated slideInUp" src="http://via.placeholder.com/115x70" />
           <img className="animated slideInLeft" src="http://via.placeholder.com/115x70" />
         </div>
+        {/*Progressbar*/}
         <div className="progress animated rotateInUpRight">
           <div className="progressBar">0%</div>
         </div>
@@ -21,4 +26,5 @@ class Photos extends Component {
     );
   }
 }
+
 export default Photos;
