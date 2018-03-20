d06b1cd5062726355c6ab20241680cb2bd4a910e
laithmassoud <laythmassoud8@gmail.com>
2018-03-20 16:12:53 +0100

    testing the ModuleSideBar

--- frontend/src/components/modules-side-bar/ModulesSideBar.js
+++ frontend/src/components/modules-side-bar/ModulesSideBar.js
@@ -4,27 +4,28 @@ import React, { Component } from 'react';
 
 
 class ModulesSideBar extends Component {
-    
+
 
 
   render() {
-    let icon = (<p>event icon</p> )
-    if(this.props.data.type === 'video'){
-      icon =(<p>video picture</p>)
-    } else if (this.props.data.type === 'image'){
-      icon =(<p>image picture</p>)
-    }
-   
+
+  let icon = (<p>event icon</p> )
+  
+  if(this.props.data.type === 'video'){
+    icon =(<p>video picture</p>)
+  } else if (this.props.data.type === 'image'){
+    icon =(<p>image picture</p>)
+  }
     return (
       <div className="ModulesSideBar">
-    <h4>{this.props.data.title}</h4>
+      <h4>{this.props.data.title}</h4>
       <p>{this.props.data.description}</p>
       <img src={this.props.data.iconUrl} />
       <div className="picture-container">
       {icon}
       </div>
       <div className="date-container">
-      <p>start at: {this.props.data.displayDate} </p>
+        <p>start at: {this.props.data.displayDate} </p>
        <p>start at: {this.props.data.expiryDate} </p>
       </div>
       </div>
