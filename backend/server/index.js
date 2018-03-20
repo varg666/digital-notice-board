2ad27880c21d80f68f9eb3a73df7278a090a21a2
Mohamed Saleh <mohmed_bakry51@yahoo.com>
2018-03-20 10:29:19 +0100

    DB Connection Esthlished, Added DB URI in .env File

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
 const mockupJson = require("./mockup/mockup.json");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 
+// Using Body Parser
+app.use(bodyParser());
 
+// Connection to DB using .env File
+mongoose.connect(process.env.DB_URI);
+var db = mongoose.connection;
+ console.log(process.env.DB_URI);
+db.once("open", () => {
+	console.log("DB Connection esthablised");
+});
+
+db.on("error", (errr) => {
+	console.log(err);
+})
+
+// Main Route
 app.get('/',(req,res) => {
   res.send(mockupJson);
 });
 
+
+// Server Port
+const PORT = process.env.PORT || 4000;
 app.listen(PORT,() => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
