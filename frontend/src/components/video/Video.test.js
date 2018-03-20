3397cd34ae84f2420ee9345a4c17afc4122ed5df
Unknown <ahmedelbehary0@gmail.com>
2018-03-20 13:30:11 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- frontend/src/components/video/Video.test.js
+++ frontend/src/components/video/Video.test.js
@@ -6,5 +6,13 @@ import Video from './Video';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Video />, div);
+  var video = mount(<Video />)
+  const p = video.find('.App-title');
+  expect(p.text()).toBe('Welcome to React');
+
+  const youTubeEmbed = video.find('.embed');
+  expect(youTubeEmbed.logo().)
+
+  ReactDOM.render(video, div);
   ReactDOM.unmountComponentAtNode(div);
 });
