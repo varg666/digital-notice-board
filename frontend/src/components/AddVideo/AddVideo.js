8c63c34e0282cac2c5b27cab57734dd671850f67
Boris Lichtenstein <boimte@t-online.de>
2018-03-21 16:14:23 +0100

    refactored functionality for sending input values

--- frontend/src/components/AddVideo/AddVideo.js
+++ frontend/src/components/AddVideo/AddVideo.js
@@ -1,70 +1,32 @@
 import React from 'react';
 import moment from 'moment';
-import 'moment/locale/de';
 
 
 class AddVideo extends React.Component {
 
-  state = {
-    title: null,
-    description: null,
-    displayDate: null,
-    expiryDate: null,
-    textarea: null
-  };
-
-  sendInfo = (e) => {
-    e.preventDefault();
-    console.log(this.state);
-  }
-
-  sendingTitle = e => {
-    this.setState({
-    title: e.target.value
-    })
-  }
-
-  sendingDesc = e => {
-    this.setState({
-    description: e.target.value
-    })
-  }
-
-  sendingDateStart = e => {
-    let startDate = e.target.value;
-    startDate = moment(startDate).format('L');
-    this.setState({
-    displayDate: startDate
-    })
-  }
-
-  sendingDateEnd = e => {
-    let endDate = e.target.value;
-    endDate = moment(endDate).format('L');
-    this.setState({
-    expiryDate: endDate
-    })
-  }
-  
-  sendingVideo = e => {
-    this.setState({
-    textarea: e.target.value
-    })
+ sendInfo = (e) => {
+    e.preventDefault()
+    var form = {}
+    for(let i = 0; i < e.target.elements.length; i++) {
+      form[e.target.elements[i].id] = e.target.elements[i].value
+    }
+    console.log(form)
+    this.setState({form: form})
   }
 
   render() {
     return (
       <form onSubmit={this.sendInfo} className="AddVideo">
         <label className="edit-title">Title</label>
-        <input onChange={this.sendingTitle} type="text"/>
+        <input id="title" onChange={this.sendingTitle} type="text"/>
         <label className="edit-desc">Description</label>
-        <input onChange={this.sendingDesc} type="text"/>
+        <input id="desc" onChange={this.sendingDesc} type="text"/>
         <label className="edit-date">Display Date</label>
-        <input onChange={this.sendingDateStart} type="date"/>
+        <input id="dates" onChange={this.sendingDateStart} type="date"/>
         <label className="edit-date">Expiry Date</label>
-        <input onChange={this.sendingDateEnd} type="date"/>
+        <input id="datee" onChange={this.sendingDateEnd} type="date"/>
         <label>Youtube Embed Code</label>
-        <input className="video-url" onChange={this.sendingVideo}/>
+        <input id="url" className="video-url" onChange={this.sendingVideo}/>
         <a href="#">Delete this Video</a>
         <button type="submit">Add</button>
       </form>
