a8ce2b1768720afd024dc762eacdfaa8fd2d5e7a
Tommy <spiel.hoelle@live.de>
2018-03-21 21:29:06 +0100

    populate backend data from server, remove console.log

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,7 +4,14 @@ const PORT = process.env.PORT || 4000;
 const mockupJson = require("./mockup/mockup.json");
 
 
-app.get('/',(req,res) => {
+app.use(function (req, res, next) {
+    // TODO restrict access to final domain
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/',(req,res,next) => {
   res.send(mockupJson);
 });
 
--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -30,70 +30,23 @@ class Admin extends Component {
             "src": ''
         }
     
-},
-        data:
-        [
-    {
-        "type": "VIDEO",
-        "iconUrl": "https://png.icons8.com/ios/50/000000/play-button-circled-filled.png",
-        "title": "The Rabbit and the turle",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "description": "",
-        "content":
-        {
-            "src": "https://pixabay.com/en/background-image-colorful-art-967820/"
-        }
     },
-    {
-        "type": "PHOTOS",
-        "iconUrl": "https://png.icons8.com/ios-glyphs/50/000000/picture.png",
-        "title": "Startup Trip",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content":
-        {
-            "src": ["https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/"]
-        }
-    },
-    {
-        "type": "CODE",
-        "iconUrl": "https://png.icons8.com/ios/50/000000/code-file-filled.png",
-        "title": "The Rabbit and the turle",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content":
-        {
-            "codeImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
-            "authorImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
-            "authorName": "Aemal Surname"
-        }
-
-    },
-    {
-        "type": "ANNOUNCEMENT",
-        "iconUrl": "https://png.icons8.com/ios-glyphs/50/000000/megaphone.png",
-        "title": "19th of March to 22nd march",
-        "subtitle": "Devugees Project Week",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content": {
-                "street": "Vulkanstr. 1",
-                "postal": "11000",
-                "city": "Berlin"
-            }
-        }],
+    data: [],
            youtubeCode: ['HCnGKF_Ro2A']
       
         }   
 }    
 
+componentDidMount() {
+  fetch(`http://localhost:4000`)
+      .then(resp => resp.json())
+      .then((data) => {
+        this.setState({data: data})
+      })
+  }
 handleClick = (item)=>{
 
-  console.log(item)
+  //cnsole.log(item)
     this.setState({ current:
       {
         "type": item.type,
@@ -111,7 +64,7 @@ handleClick = (item)=>{
 
 }
 componentDidUpdate() {
-    console.log(this.state)
+    //console.log(this.state)
 
 }
 
