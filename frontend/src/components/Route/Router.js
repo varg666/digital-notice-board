ad4b913de81140923317de48c5cc87f584d77f64
Tommy <spiel.hoelle@live.de>
2018-03-22 16:31:34 +0100

    add admin route and fix button

--- frontend/src/components/Route/Router.js
+++ frontend/src/components/Route/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ModulesSideBar from '../modules-side-bar/ModulesSideBar';
 import App from '../../App'
+import Admin from '../Admin/Admin'
 import Login from '../login/Login';
 import Register from '../Register/Register';
 import ForgotPassword from '../forgot/ForgotPassword';
@@ -11,6 +12,7 @@ const Router = () => (
   <BrowserRouter>
     <Switch>
      <Route exact path='/' component={App} />
+     <Route exact path='/admin' component={Admin} />
      <Route exact path='/admin/login' component={Login} />
      <Route exact path='/admin/Register' component={Register} />
      <Route exact path='/admin/reset' component={ForgotPassword} />
