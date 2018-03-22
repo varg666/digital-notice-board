54d7e8b7bfaa287cd7e2ab6f6eb15435f2c1caf1
Nacho <nacho@nachomerino.com>
2018-03-22 10:38:39 +0100

    deleted dummy data, now data came from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,76 +15,21 @@ import { Button, Nav, NavItem, NavLink } from 'reactstrap';
 
 class App extends Component {
 
-      constructor(props){
-      super(props);
-      this.state = {
-        data:
-        [
-    {
-        "type": "VIDEO",
-        "iconUrl": "https://png.icons8.com/ios/50/000000/play-button-circled-filled.png",
-        "title": "The Rabbit and the turle",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "description": "",
-        "content":
-        {
-            "src": "https://pixabay.com/en/background-image-colorful-art-967820/"
-        }
-    },
-    {
-        "type": "PHOTOS",
-        "iconUrl": "https://png.icons8.com/ios-glyphs/50/000000/picture.png",
-        "title": "Startup Trip",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content":
-        {
-            "src": ["https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/", "https://pixabay.com/en/background-image-colorful-art-967820/"]
-        }
-    },
-    {
-        "type": "CODE",
-        "iconUrl": "https://png.icons8.com/ios/50/000000/code-file-filled.png",
-        "title": "The Rabbit and the turle",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content":
-        {
-            "codeImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
-            "authorImage": "https://pixabay.com/en/background-image-colorful-art-967820/",
-            "authorName": "Aemal Surname"
-        }
-
-    },
-    {
-        "type": "ANNOUNCEMENT",
-        "iconUrl": "https://png.icons8.com/ios-glyphs/50/000000/megaphone.png",
-        "title": "19th of March to 22nd march",
-        "subtitle": "Devugees Project Week",
-        "description": "Team work video",
-        "expiryDate": "01.07.2018",
-        "displayDate": "01.07.2018",
-        "content": {
-                "street": "Vulkanstr. 1",
-                "postal": "11000",
-                "city": "Berlin"
-            }
-        }],
-           youtubeCode: ['HCnGKF_Ro2A']
-
-        }
-}
+  constructor(props){
+  super(props);
+  this.state = {
+    data: [],
+    youtubeCode: ['HCnGKF_Ro2A']
+    }
+  }
 
   componentDidMount() {
     fetch(`http://localhost:4000`).then(resp => resp.json()).then((data) => {
       this.setState({data: data})
     })
-
   }
-endingHandler = () => {
+
+  endingHandler = () => {
     console.log("The video has ended");
     }
 
