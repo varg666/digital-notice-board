b88ee70a6249146621305425d0de35d8fd4db64a
h <mar.ha17@outlook.com>
2018-03-22 14:18:39 +0100

    create the clickhandler for slide

--- frontend/src/components/modules-side-bar/ModulesSideBar.js
+++ frontend/src/components/modules-side-bar/ModulesSideBar.js
@@ -17,12 +17,10 @@ class ModulesSideBar extends Component {
 
        } 
 
-   this.handleToggleClick= this.handleToggleClick.bind(this);
+}
 
 }
-  handleToggleClick(e){
-    console.log(e)
-  }
+ 
   render(props) {
     let icon;
     if(this.props.data.type === 'video'){
@@ -32,18 +30,17 @@ class ModulesSideBar extends Component {
     }
    
     return (
-      <div className="ModulesSideBar slide-selected" onClick={this.props.onclick}>
-        <h4 onClick={this.props.onclick}>{this.props.data.title}</h4>
-        <p onClick={this.props.onclick}>{this.props.data.description}</p>
-        <img onClick={this.props.onclick} src={getIcons(this.props.data.type)} />
-        <h4  className='slide-text' onClick={this.props.onclick}>{this.props.data.title}</h4>
-        </div>
-        <p className='slide-desc' onClick={this.props.onclick}>{this.props.data.description}</p>
+      <div className="ModulesSideBar slide-selected" onClick={(e)=>this.props.handleToggleClick(this)}>
+        <h4>{this.props.data.title}</h4>
+        <p>{this.props.data.description}</p>
+        <img src={getIcons(this.props.data.type)} />
+        <h4  className='slide-text'>{this.props.data.title}</h4>
+        <p className='slide-desc'>{this.props.data.description}</p>
         <div className="date-container slide-text small">
         <p className='slide-text small'>start at: {this.props.data.displayDate} </p>
          <p className='slide-text small'>start at: {this.props.data.expiryDate} </p>
         </div>
-      </div>
+        </div>
      
     );
   }
