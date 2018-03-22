6d2dff6b0e6bbebb6c3669e0a36fd733ed521c2d
Mohamed Saleh <mohmed_bakry51@yahoo.com>
2018-03-22 14:00:35 +0100

    added style for login and register and reset passrword

--- frontend/src/components/Route/Router.js
+++ frontend/src/components/Route/Router.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ModulesSideBar from '../modules-side-bar/ModulesSideBar';
 import App from '../../App'
 import Login from '../login/Login';
-import Admin from '../Admin/Admin';
+import Register from '../Register/Register';
+import ForgotPassword from '../forgot/ForgotPassword';
 
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
      <Route exact path='/' component={App} />
-     <Route exact path='/login' component={Login} />
-     <Route exact path='/admin:id' component={Admin} />
-
+     <Route exact path='/admin/login' component={Login} />
+     <Route exact path='/admin/Register' component={Register} />
+     <Route exact path='/admin/reset' component={ForgotPassword} />
     </Switch>
   </BrowserRouter>
 
