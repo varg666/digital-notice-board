8e0edc549e4704defc5cd7ca0d3b727d8a8f18f7
Boris Lichtenstein <boimte@t-online.de>
2018-03-22 15:53:46 +0100

    added post methos to add a video, still bugs, obj is not sending correctly to db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Login from './components/login/Login.js';
 import Register from './components/Register/Register.js';
 import Photos from './components/photos/Photos.js';
@@ -12,16 +12,16 @@ import getIcons from './constants/icons.js'
 import SlideTimeline from './components/timeline/SlideTimeline.js';
 import SnippetSlide from './components/snippet-slide/SnippetSlide.js';
 import moment from 'moment';
-import { Button, Nav, NavItem, NavLink } from 'reactstrap';
+import {Button, Nav, NavItem, NavLink} from 'reactstrap';
 
 class App extends Component {
 
-  constructor(props){
-  super(props);
-  this.state = {
-    data: [],
-    youtubeCode: ['qJz18c6gw8c'],
-    currentSlide: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      youtubeCode: ['qJz18c6gw8c'],
+      currentSlide: 0
     }
   }
 
@@ -34,9 +34,9 @@ class App extends Component {
 
   endingHandler = () => {
     console.log("The video has ended");
-    }
+  }
 
-  slideHandler (slide) {
+  slideHandler(slide) {
     console.log(this.state.currentSlide)
     console.log(slide.props.data);
     this.setState({currentSlide: slide.props.data})
@@ -45,55 +45,70 @@ class App extends Component {
 
   sendInfo = (e) => {
     e.preventDefault()
-    var form = {}
-    for(let i = 0; i < e.target.elements.length; i++) {
-    if(e.target.elements[i].value !== "") {
+    const form = {};
+    for (let i = 0; i < e.target.elements.length; i++) {
+      if (e.target.elements[i].value !== "") {
         form[e.target.elements[i].id] = e.target.elements[i].value
       }
     }
+    console.log('this a fetach', form)
+    //TODO POST method is sending the object not correctly
     this.setState({form: form})
+    fetch('http://localhost:4000/admin/add', {
+      body: JSON.stringify(form),
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      referrer: 'no-referrer'
+    }).then(res => res.json()).catch(error => console.error('Error:', error)).then(response => console.log('Success:', response));
+  };
+
+}
+handleToggleClick(e) {
+  console.log(e)
+}
+render() {
+  console.log(this.state.currentSlide)
+
+  if (this.state.currentSlide.type === "video") {
+    var content = <Video youtubeCode={this.state.youtubeCode} endingHandler={() => {
+      this.endingHandler()
+    }}/>
+  } else {
+    var content = "hey"
   }
- handleToggleClick(e){
-    console.log(e)
-  }
-  render() {
-    console.log(this.state.currentSlide)
+  return (
+    <div className="App">
+      <Nav>
+        <NavItem>
+          <NavLink href="admin1">
+            <Button color="primary">Admin</Button>
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <div className="skewed"></div>
+      <div>
+        <h1 id="title">Digital-notice-board</h1>
+      </div>
 
-    if(this.state.currentSlide.type === "video"){
-          var content = <Video youtubeCode={this.state.youtubeCode} endingHandler={() => {this.endingHandler()}} /> 
-        }
-         else {
-          var content = "hey"
-         }
-return (
-<div className="App">
-<Nav>
-<NavItem>
-<NavLink href="admin1">
-<Button color="primary">Admin</Button>
-</NavLink>
-</NavItem>
-</Nav>
-<div className="skewed"></div>
-<div>
-<h1 id="title">Digital-notice-board</h1>
-</div>
+      <div className='column1'>
+        {content}
 
-<div className='column1'>
-{content}
+      </div>
 
-</div>
+      <div className='column2'>
+        {this.state.data.map((item, value) => <ModulesSideBar key={value} data={item} handleToggleClick={this.slideHandler.bind(this)}/>)}
+      </div>
 
-<div className='column2'>
-{this.state.data.map((item,value) => <ModulesSideBar key={value} data={item}  
-  handleToggleClick={this.slideHandler.bind(this)} /> )}
-</div>
+      <h1>And here come the other components</h1>
 
-<h1>And here come the other components</h1>
-
-
-</div>
-);
+    </div>
+  );
 }
 }
 
