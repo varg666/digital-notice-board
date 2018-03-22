bab0d4be5f2f4a9cc63cd12887656aa972a326cf
Unknown <sabine.gottfried@gmail.com>
2018-03-22 15:11:36 +0100

    Show all slides in database without any filtering

--- backend/server/routes/admin.js
+++ backend/server/routes/admin.js
@@ -36,15 +36,22 @@ const ensureAuthenticated  = (req, res, next) => {
   }
 };
 
+// Admin Home Route
 router.get('/', ensureAuthenticated, (req, res) => {
-  res.json({"success": "You are signed into the admin panel."});
+  Content.find({}, (err, slides) => {
+      if (err) {
+          console.log(err)
+      } else {
+          res.send(slides);
+      } 
+  })
 });
 
-// Add New Slide
+// Add New Slide Route
 router.post('/add', ensureAuthenticated, addSlide);
 
-// Add New Slide
-router.put('/edit/:id', (req, res) => {
+// Edit New Slide Route
+router.put('/edit/:id', ensureAuthenticated, (req, res) => {
   Content.findById(req.params.id, function(err, Content) {
     if(!Content)
       return res.send({err: 'Content not found'});
@@ -74,7 +81,7 @@ router.put('/edit/:id', (req, res) => {
   });
 });
 
-// delete Slide
+// Delete Slide
 router.delete('/delete/:id', ensureAuthenticated, (req,res) => {
   Content.findById(req.params.id, function(err, Content) {
     if(!Content)
