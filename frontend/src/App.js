05118dcdcc41a48f5dbdf9ea50ced10697546faf
Tommy <spiel.hoelle@live.de>
2018-03-22 19:21:18 +0100

    fix bugs from merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,63 +44,40 @@ class App extends Component {
   sendInfo = (e) => {
     e.preventDefault()
     const form = {};
-    for(let i = 0; i < e.target.elements.length; i++) {
-    if(e.target.elements[i].value !== "") {
+    for (let i = 0; i < e.target.elements.length; i++) {
+      if (e.target.elements[i].value !== "") {
         form[e.target.elements[i].id] = e.target.elements[i].value
       }
     }
-    console.log('this a fetach' ,form)  
+    console.log('this a fetach', form)
     //TODO POST method is sending the object not correctly
     this.setState({form: form})
-        fetch('http://localhost:4000/admin/add', {
-            body: JSON.stringify(form), 
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-            },
-            method: 'POST', 
-            mode: 'cors', 
-            redirect: 'follow', 
-            referrer: 'no-referrer', 
-            })
-            .then(res => res.json())
-            .catch(error => console.error('Error:', error))
-            .then(response => console.log('Success:', response));
-    };
-
-    if(this.state.currentSlide.type === "video"){
-          var content = <Video youtubeCode={this.state.youtubeCode} endingHandler={() => {this.endingHandler()}} /> 
-        }
-         else {
-          var content = "hey"
-         }
-return (
-<div className="App">
-<Nav>
-<NavItem>
-<NavLink href="admin">
-<Button color="primary">Admin</Button>
-</NavLink>
-</NavItem>
-</Nav>
-<div className="skewed"></div>
-<div>
-<h1 id="title">Digital-notice-board</h1>
-</div>
-
-  if (this.state.currentSlide.type === "video") {
-    var content = <Video youtubeCode={this.state.youtubeCode} endingHandler={() => {
-      this.endingHandler()
-    }}/>
-  } else {
-    var content = "hey"
+    fetch('http://localhost:4000/admin/add', {
+      body: JSON.stringify(form),
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      referrer: 'no-referrer'
+    }).then(res => res.json()).catch(error => console.error('Error:', error)).then(response => console.log('Success:', response));
   }
-  return (
-    <div className="App">
+
+
+  render() {
+    if (this.state.currentSlide.type === "video") {
+      var content = <Video youtubeCode={this.state.youtubeCode} endingHandler={() => this.endingHandler()}/>
+    } else {
+      var content = "hey"
+    }
+    return (
+      <div className="App">
       <Nav>
         <NavItem>
-          <NavLink href="admin1">
+          <NavLink href="admin">
             <Button color="primary">Admin</Button>
           </NavLink>
         </NavItem>
@@ -112,28 +89,15 @@ return (
 
       <div className='column1'>
         {content}
-
       </div>
 
       <div className='column2'>
+        <h1>And here come the other components</h1>
         {this.state.data.map((item, value) => <ModulesSideBar key={value} data={item} handleToggleClick={this.slideHandler.bind(this)}/>)}
       </div>
-
-      <h1>And here come the other components</h1>
-
+    
     </div>
-  );
+    );
+  }
 }
-}
-
-/*
-<ForgotPassword />
-<Login  />
-<Register  />
-<AddVideo sendChildInfo={this.sendInfo.bind(this)}/>
-<SlideTimeline />
-<SnippetSlide image="https://images.pexels.com/photos/60204/pexels-photo-60204.jpeg?h=350&auto=compress&cs=tinysrgb"
-profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlZ4wEIMhORQAr9rv15Mj5zZt_t4rw_bmlPLTSdh9ocK9zhF8"/>
-*/
-
 export default App;
