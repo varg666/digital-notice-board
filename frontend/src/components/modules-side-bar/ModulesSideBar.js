01c7950a9e8de770012fb07dcfd67b2de6ca5354
Tommy <spiel.hoelle@live.de>
2018-03-26 01:12:46 +0200

    add font-awesome icons and show nice sidebar

--- frontend/src/components/modules-side-bar/ModulesSideBar.js
+++ frontend/src/components/modules-side-bar/ModulesSideBar.js
@@ -5,20 +5,35 @@ import getIcons from '../../constants/icons.js';
 class ModulesSideBar extends Component {
 
   render(props) {
+  console.log(this.props.data.type.toLowerCase());
     let icon;
-    if(this.props.data.type === 'video'){
-      icon =(<p>video picture</p>)
-    } else if (this.props.data.type === 'image'){
-      icon =(<p>image picture</p>)
+    //TODO the types are here defined, but they should come from 
+    if(this.props.data.type.toLowerCase() === 'video'){
+      icon =( <i className="fa fa-youtube fa-4x"></i>)
+    } else if (this.props.data.type.toLowerCase() === 'photos'){
+      icon =( <i className="fa fa-camera-retro fa-4x"></i>)
+    } else if (this.props.data.type.toLowerCase() === 'code'){
+      icon =( <i className="fa fa-code fa-4x"></i>)
+    } else if (this.props.data.type.toLowerCase() === 'announcement'){
+      icon =( <i className="fa fa-calendar fa-4x"></i>)
+    } else if (this.props.data.type.toLowerCase() === 'repo'){
+      icon =( <i className="fa fa-github fa-4x"></i>)
     }
    
     return (
-      <div className={ `card p-3 mb-3${this.props.current._id === this.props.data._id ? " slide-selected" : ""}`} onClick={(e)=>this.props.handleToggleClick(this)}>
-        <h4>{this.props.data.title}</h4>
-        <p>{this.props.data.description}</p>
-        <img src={getIcons(this.props.data.type)} />
+      <div className={ `type_${this.props.data.type.toLowerCase()} card p-3 mb-3${this.props.current._id === this.props.data._id ? " slide-selected" : ""}`} onClick={(e)=>this.props.handleToggleClick(this)}>
+        <div className="d-flex flex-column flex-md-row align-items-center">
+          <div className="mr-md-3">
+            {icon}
+          </div>
+          <div className="text ">
+            <h4 className="">{this.props.data.title}</h4>
+            {
+             }
+             </div>
         </div>
-     
+      </div>
+
     );
   }
 }
