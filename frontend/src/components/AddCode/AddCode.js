bb89a4a90b47ac09ec27d11ed3647a5ba7f290f0
Patrick Schuelke <p-schuelke@gmx.de>
2018-04-04 11:50:43 +0200

    add component to edit codesnippet on admin side

---
+++ frontend/src/components/AddCode/AddCode.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback,
+  FormText
+} from 'reactstrap';
+
+class AddCode extends React.Component {
+  state = {
+    form: this.props.data,
+    fields: {},
+    errors: {}
+  }
+
+  //TODO optimize this validation and make it work for the other fields
+  //https://waffle.io/devugees/digital-notice-board/cards/5ac2807897f9dd00256b555a
+  onChange(field, value) {
+    let data = {...this.state.form};
+    data[field] = value
+    this.setState({form: data})
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.props.sendChildInfo} className="AddVideo">
+        <h1>Add Code</h1>
+        <FormGroup>
+          <Label for="examplePassword">Title</Label>
+          <Input
+            onChange={(e) => this.onChange(e.target.id, e.target.value)}
+            id="title"
+            value={this.state.form.title}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Description</label>
+          <input
+            onChange={(e) => this.onChange(e.target.id, e.target.value)}
+            className="form-control"
+            id="description"
+            type="text"
+            value={this.state.form.description}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Display Date</label>
+          <input
+            onChange={(e) => this.onChange(e.target.id, e.target.value)}
+            className="form-control"
+            id="displayDate"
+            type="date"
+            value={this.state.form.displayDate}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Expiry Date</label>
+          <input
+            onChange={(e) => this.onChange(e.target.id, e.target.value)}
+            className="form-control"
+            id="expiryDate"
+            type="date"
+            value={this.state.form.expiryDate}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Code Snippet</Label>
+          <Input
+            onChange={(e) => this.onChange(e.target.id, e.target.value)}
+            id="content"
+            type="textarea"
+            value={this.state.form.content}
+          />
+        </FormGroup>
+        <a href="#">Delete this Video</a>
+        <br/>
+        <button type="submit" className="btn btn-primary">Add</button>
+      </Form>
+    );
+  }
+}
+
+export default AddCode;
