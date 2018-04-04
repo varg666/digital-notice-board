4ace13eb68351e8325bee9e0fe63182419f5c347
Marta <martadipuccio@me.com>
2018-04-04 15:31:32 +0200

    created AddPhoto component, added in the Admin component

---
+++ frontend/src/components/AddPhoto/AddPhoto.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class AddPhoto extends React.Component {
+        constructor(props) {
+            super(props);
+        } 
+
+
+        render(props){
+            return(
+                <div>
+
+                   <p> hbajsfbjdkb</p>
+                </div>            
+            );
+        }
+}
+
+
+export default AddPhoto;
