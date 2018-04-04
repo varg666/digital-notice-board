1460c7dcaea42a5b64dd12bbd43240c81ec91cf0
Sash Seurat Samson <sash.samson@gmail.com>
2018-04-04 14:05:54 +0200

    beautify code

--- frontend/src/components/photos/Photos.js
+++ frontend/src/components/photos/Photos.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Photos.css';
 import '../../../node_modules/animate.css/animate.min.css'
 import Slides from "./slides.js"
 
 class Photos extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log(props);
   }
 
   render() {
     var images = this.props.data.content.split(',');
-console.log(images);
     return (
-      // The main container
-      <div className="photos">
-        {/*The big picture*/}
-        <img className="animated zoomIn" src={images[0]} alt="" />
-        {/*The container with slides under the big picture*/}
-        <Slides data={this.props.data}/>
-       {/*Progressbar*/}
-        <div className="progress animated rotateInUpRight">
-          <div className="progressBar"></div>
-        </div>
-      </div>
-    );
+    <div className="photos">
+      <img className="animated zoomIn" src={images[0]} alt=""/>
+      <Slides data={this.props.data}/>
+    </div>);
   }
 }
 
