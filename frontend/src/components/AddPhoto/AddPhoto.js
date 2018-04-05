fcf01c2649848b8b44bea812c28030116ba5c2c0
Marta <martadipuccio@me.com>
2018-04-05 13:36:32 +0200

    added a method to upload an image from computer

--- frontend/src/components/AddPhoto/AddPhoto.js
+++ frontend/src/components/AddPhoto/AddPhoto.js
@@ -12,18 +12,30 @@ class AddPhoto extends React.Component {
             this.state = {
               form: props,
               fields: {},
-              errors: {}
+              errors: {},
+              setOfImages : []
             }
         } 
 
-
+        onChange(field, value) {
+            let data = {...this.state.form};
+            data[field] = value;
+            this.setState = {form: data};
+        }
+        setOfImages(event){
+            event.preventDefault();
+            this.setState({
+                setOfImages: ["hello", "hi"]  
+            })
+            
+        }
         render(props){
             return(
                 <Form onSubmit={this.props.sendChildInfo}>
                     <h1>Add Photos</h1>
                     
                     <FormGroup >
-                        <Label className="col-xs-3" for="exampleText">Title</Label>
+                        <Label for="exampleText">Title</Label>
                         <Input type="text" onChange={(e) => this.onChange(e.target.id, e.target.value)} id="title" invalid={this.state.fields["title"]} valid={!this.state.fields["title"]} value={this.state.form.title} />
                         <FormFeedback>{this.state.errors["title"]}</FormFeedback>
                         <FormFeedback valid>{this.state.errors["title"]}</FormFeedback>
@@ -32,26 +44,38 @@ class AddPhoto extends React.Component {
                         <Label for="exampleText">Description</Label>
                         <Input type="text" onChange={(e) => this.onChange(e.target.id, e.target.value)} className="form-control" id="description" type="text" value={this.state.form.description} id="description" />
                     </FormGroup>
-                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">    
-                            <label className="mr-sm-2">Display Date</label>
+                    <FormGroup>    
+                            <label>Display Date</label>
                             <input onChange={(e) => this.onChange(e.target.id, e.target.value)} className="form-control" id="displayDate" type="date" value={this.state.form.displayDate}/>
                     </FormGroup>    
-                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">    
-                            <label className="mr-sm-2">Expiry Date</label>
+                    <FormGroup >    
+                            <label>Expiry Date</label>
                             <input onChange={(e) => this.onChange(e.target.id, e.target.value)} className="form-control" id="expiryDate" type="date" value={this.state.form.expiryDate}/>
                     </FormGroup>  
                     <FormGroup>
-                            <Label for="examplePassword">Photo-Link</Label>
-                            <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/>
+                            <Label for="examplePassword"></Label>
+                            <div>
+                                <input type="file" name={this.state.setOfImages} id="image-uploder" className="file-loader" multiple />
+                                <button onClick={this.setOfImages.bind(this)}>+</button>
+                                <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/> 
                             <button type="submit" className="btn btn-primary">Add Photo</button>
+                            </div>
                             <br />
-                            <Label for="examplePassword">Photo-Link</Label>
-                            <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/>
+                            <Label for="examplePassword"></Label>
+                            <div>
+                                <input type="file" name={this.state.setOfImages} id="image-uploder" className="file-loader" multiple />
+                                <button onClick={this.setOfImages.bind(this)}>+</button>
+                                <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/> 
                             <button type="submit" className="btn btn-primary">Add Photo</button>
+                            </div>
                             <br />
-                            <Label for="examplePassword">Photo-Link</Label>
-                            <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/>
+                            <Label for="examplePassword"></Label>
+                            <div>
+                                <input type="file" name={this.state.setOfImages} id="image-uploder" className="file-loader" multiple />
+                                <button onClick={this.setOfImages.bind(this)}>+</button>
+                                <Input onChange={(e) => this.onChange(e.target.id, e.target.value)} id="content" invalid={this.state.fields["content"]} valid={!this.state.fields["content"]} value={this.state.form.content}/> 
                             <button type="submit" className="btn btn-primary">Add Photo</button>
+                            </div>
 
                             <FormFeedback>{this.state.errors["content"]}</FormFeedback>
                             <FormFeedback valid>{this.state.errors["content"]}</FormFeedback>
