fa58e76ab9caa66a8b9c065f40f8cf9e2df83477
Marta <martadipuccio@me.com>
2018-04-10 09:46:39 +0200

    Button imported twice by mistake
    deleted

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -7,7 +7,7 @@ import AddCode from '../AddCode/AddCode';
 import AddPhoto from '../AddPhoto/AddPhoto';
 import { Button, Nav, NavItem, NavLink } from 'reactstrap';
 import AdminNavigation from './AdminNavigation';
-import { Button } from 'reactstrap';
+
 import axios from 'axios';
 import AddAnnouncement from '../AddAnnouncement/AddAnnouncement';
 import AddGithub from '../AddGithub/AddGithub';
