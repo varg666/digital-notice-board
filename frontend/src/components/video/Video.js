a553a1e65b0a3b60685c587f159f816960e5072c
Elbehary <ahmedelbehary0@gmail.com>
2018-04-11 15:14:18 +0200

    haha

--- frontend/src/components/video/Video.js
+++ frontend/src/components/video/Video.js
@@ -3,14 +3,16 @@ import ReactPlayer from "react-player";
 import "./Video.css";
 
 const Video = props => {
+  console.log(props)
   return (
     <div className="slide-body">
+    <h2>{props.data.title}</h2>
       <ReactPlayer
         width="100%"
         height="80vh"
         onDuration={duration => props.setVideoDuration(duration)}
         onEnded={props.endingHandler}
-        url={`https://www.youtube.com/embed/${props.youtubeCode}`}
+        url={`https://www.youtube.com/embed/${props.data.content}`}
         playing
       />
     </div>
