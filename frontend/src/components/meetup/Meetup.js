08ce7e390eb4d9635a9ff6895999a4e59dbd3792
h <mar.ha17@outlook.com>
2018-04-11 13:52:17 +0200

    added  the fetch for meetup API

--- frontend/src/components/meetup/Meetup.js
+++ frontend/src/components/meetup/Meetup.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Meetup extends Component {
+	 constructor(props) {
+    super(props);
+    this.state = {
+      events: {}
+      
+    }
+  }
+   componentDidMount() {
+   	fetch(`https://api.meetup.com/How-to-become-a-Web-Developer/events?photo-host=public&page=20&sig_id=206185798&sig=22ed3febfbf5564af76e434947acbd370dd7f051`)
+    .then(resp => resp.json())
+    .then((data) => {
+       console.log(data)
+       this.setState({events:data})
+       
+     });
+    
+  }
 	render(props) {
 		return(
 		<div>
-			
+		{this.state.events.map(event => {
+			console.log(event);
+		})}
 		</div>
 		);
 	}
