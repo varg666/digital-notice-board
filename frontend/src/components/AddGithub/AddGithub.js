a96808f83cb19593a4f27de17d3cbd94615c6883
Sash Seurat Samson <sash.samson@gmail.com>
2018-04-11 10:06:54 +0200

    Changed class="" to className=""

--- frontend/src/components/AddGithub/AddGithub.js
+++ frontend/src/components/AddGithub/AddGithub.js
@@ -26,46 +26,46 @@ class AddGithub extends React.Component {
         		<h1>devugees-digital-notice-board</h1>
     			<FormGroup>
 		          <Label for="examplePassword">Title</Label>
-		          <Input 
+		          <Input
 		          onChange={(e) => this.onChange(e.target.id, e.target.value)}
-		          id="title" invalid={this.state.fields["title"]} 
+		          id="title" invalid={this.state.fields["title"]}
 		          valid={!this.state.fields["title"]} value={this.state.form.title}/>
 		          <FormFeedback>{this.state.errors["title"]}</FormFeedback>
-		          <FormFeedback valid>{this.state.errors["title"]}</FormFeedback>  
+		          <FormFeedback valid>{this.state.errors["title"]}</FormFeedback>
 		        </FormGroup>
 
 		        <label>Description</label>
 		        <input
 				onChange={(e) => this.onChange(e.target.id, e.target.value)}
-				className="form-control" id="description" 
-				type="text" 
+				className="form-control" id="description"
+				type="text"
 				value={this.state.form.description}/>
 
 		        <label>Display Date</label>
 		        <input
-		        onChange={(e) => this.onChange(e.target.id, e.target.value)} 
-		        className="form-control" id="displayDate" 
+		        onChange={(e) => this.onChange(e.target.id, e.target.value)}
+		        className="form-control" id="displayDate"
 		        type="date"
 				value={this.state.form.displayDate}/>
 
 		        <label>Expiry Date</label>
-		        <input 
+		        <input
 		        onChange={(e) => this.onChange(e.target.id, e.target.value)}
 		        className="form-control" id="expiryDate"
-		        type="date" 
+		        type="date"
 		        value={this.state.form.expiryDate}/>
 
 		        <FormGroup>
 		          <Label for="examplePassword">Content</Label>
-		          <Input 
-		          onChange={(e) => this.onChange(e.target.id, e.target.value)} 
-		          id="content" invalid={this.state.fields["content"]} 
+		          <Input
+		          onChange={(e) => this.onChange(e.target.id, e.target.value)}
+		          id="content" invalid={this.state.fields["content"]}
 		          valid={!this.state.fields["content"]} value={this.state.form.content}/>
 		          <FormFeedback>{this.state.errors["content"]}</FormFeedback>
 		          <FormFeedback valid>{this.state.errors["content"]}</FormFeedback>
 		        </FormGroup>
-		        
-		        	<div class="d-flex justify-content-between">
+
+		        	<div className="d-flex justify-content-between">
 		        		<a href="#">Delete this Link</a>
 		        		<button type="submit" className="btn btn-primary">Add Github Link</button>
 		        	</div>
@@ -74,5 +74,3 @@ class AddGithub extends React.Component {
 	}
 }
 export default AddGithub;
-
-
