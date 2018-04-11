54ce30a598e25faa9c0891f66bbb986575433c43
h <mar.ha17@outlook.com>
2018-04-11 14:50:47 +0200

    rendered the object for API

--- frontend/src/components/meetup/Meetup.js
+++ frontend/src/components/meetup/Meetup.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class Meetup extends Component {
-	 constructor(props) {
+	constructor(props) {
     super(props);
     this.state = {
       events: {}
       
     }
-  }
-   componentDidMount() {
-   	fetch(`https://api.meetup.com/How-to-become-a-Web-Developer/events?photo-host=public&page=20&sig_id=206185798&sig=22ed3febfbf5564af76e434947acbd370dd7f051`)
+}
+   	componentDidMount() {
+   	fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/How-to-become-a-Web-Developer/events?photo-host=public&page=20&sig_id=206185798&sig=22ed3febfbf5564af76e434947acbd370dd7f051`)
     .then(resp => resp.json())
     .then((data) => {
        console.log(data)
@@ -17,8 +17,16 @@ class Meetup extends Component {
        
      });
     
-  }
+}
+
+
 	render(props) {
+if(this.state.events.length > 0) {
+		this.state.events.map(event => {
+			console.log(event);
+		})
+}
+	
 		return(
 		<div>
 		
